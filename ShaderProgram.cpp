cc614c409e583ba6a9626fe7d816ebdeb5215859
Dmitrii Ivanov <dmitrii.ivanov@tul.cz>
2025-05-17 12:17:23 +0200

    full linux support

--- ShaderProgram.cpp
+++ ShaderProgram.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
--- gl_err_callback.h
+++ gl_err_callback.h
@@ -2,7 +2,11 @@
 #define __gl_err_callback_h__
 
 #include <GL/glew.h> 
-#include <GL/wglew.h> 
+#ifdef _WIN32
+    #include <GL/wglew.h>  // Windows-specific WGL extensions
+#elif defined(__linux__)
+    #include <GL/glxew.h>  // Linux-specific GLX extensions
+#endif
 #include <GLFW/glfw3.h>
 
 void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
