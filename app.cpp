3698fe51e7cecabf34f1a0a5b45d745bbe747ca1
Kyrylo Yefremov <kyrylo.yefremov@tul.cz>
2025-05-17 14:42:13 +0200

    add texture init and draw

--- app.cpp
+++ app.cpp
@@ -98,6 +98,12 @@ void App::printGLInfo() {
 }
 
 bool App::init() {
+	// initialize GLFW window hints
+    glEnable(GL_DEPTH_TEST);
+
+    // assume ALL objects are non-transparent 
+    glEnable(GL_CULL_FACE);
+
     // request debug context
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
@@ -172,11 +178,21 @@ bool App::init() {
 
 void App::initAssets(void) {
     // load shader program
-    shader = ShaderProgram("resources/shaders/basic_core.vert", "resources/shaders/basic_core.frag");
+    shader = ShaderProgram("resources/shaders/tex.vert", "resources/shaders/tex.frag");
 
     // load model
     Model triangleModel("resources/objects/triangle.obj", shader);
     triangleModel.origin = glm::vec3(0.0f, 0.0f, 0.0f);  // center the model
+
+    // load texture
+    GLuint texture = textureInit("resources/textures/box.png");
+
+
+    // assign all textures to all meshes
+    for (auto& mesh : triangleModel.meshes) {
+        mesh.texture_id = texture;
+    }
+
     // add to scene
     scene.emplace("triangle", std::move(triangleModel));
 }
@@ -188,9 +204,68 @@ void App::updateProjection() {
     );
 }
 
-int App::run() {
-    glEnable(GL_DEPTH_TEST);
+GLuint App::textureInit(const std::filesystem::path& file_name)
+{
+    cv::Mat image = cv::imread(file_name.string(), cv::IMREAD_UNCHANGED);  // Read with (potential) Alpha
+    if (image.empty()) {
+        throw std::runtime_error("No texture in file: " + file_name.string());
+    }
 
+    // or print warning, and generate synthetic image with checkerboard pattern 
+    // using OpenCV and use as a texture replacement 
+
+    GLuint texture = gen_tex(image);
+
+    return texture;
+}
+
+GLuint App::gen_tex(cv::Mat& image)
+{
+    GLuint ID = 0;
+    if (image.empty())
+        throw std::runtime_error("Image empty?\n");
+    
+
+    // Generates an OpenGL texture object
+    glCreateTextures(GL_TEXTURE_2D, 1, &ID);
+
+    switch (image.channels()) {
+    case 3:
+        // Create and clear space for data - immutable format
+        glTextureStorage2D(ID, 1, GL_RGB8, image.cols, image.rows);
+        // Assigns the image to the OpenGL Texture object
+        glTextureSubImage2D(ID, 0, 0, 0, image.cols, image.rows, GL_BGR, GL_UNSIGNED_BYTE, image.data);
+        break;
+    case 4:
+        glTextureStorage2D(ID, 1, GL_RGBA8, image.cols, image.rows);
+        glTextureSubImage2D(ID, 0, 0, 0, image.cols, image.rows, GL_BGRA, GL_UNSIGNED_BYTE, image.data);
+        break;
+    default:
+        throw std::runtime_error("unsupported channel cnt. in texture:" + std::to_string(image.channels()));
+    }
+
+    // Configures the type of algorithm that is used to make the image smaller or bigger
+    // nearest neighbor - ugly & fast 
+    //glTextureParameteri(ID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);  
+    //glTextureParameteri(ID, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    // bilinear - nicer & slower
+    //glTextureParameteri(ID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);    
+    //glTextureParameteri(ID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    // MIPMAP filtering + automatic MIPMAP generation - nicest, needs more memory. Notice: MIPMAP is only for image minifying.
+    glTextureParameteri(ID, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // bilinear magnifying
+    glTextureParameteri(ID, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); // trilinear minifying
+    glGenerateTextureMipmap(ID);  //Generate mipmaps now.
+
+    // Configures the way the texture repeats
+    glTextureParameteri(ID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTextureParameteri(ID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    return ID;
+}
+
+int App::run() {
     // Enable back-face culling to improve performance by not rendering polygons facing away from the camera
     //glCullFace(GL_BACK);
     //glEnable(GL_CULL_FACE);
